236acc9dc7c6788654f363124a961f9dbea021de
jaakko <jaakko.o.pasanen@gmail.com>
2016-04-09 11:27:20 +0300

    Speedied up the cct calculation with pregenerated approximation table.

--- Matlab/uvToCct.m
+++ Matlab/uvToCct.m
@@ -1,4 +1,4 @@
-function [ cct, d_min ] = uvToCct( uv )
+function [ cct, d_min, cctR ] = uvToCct( uv )
 %UVTOCCT Finds correlated color temperature from CIE1960 color coordinates
 %Syntax
 %   [cct, d_min] = uvToCct(u, v)
@@ -14,8 +14,22 @@ if isempty(cie1960PlanckianLocusUv)
     load('cie.mat', 'cie1960PlanckianLocusUv');
 end
 
+persistent cie1960Cct;
+if isempty(cie1960Cct)
+    load('cie.mat', 'cie1960Cct');
+end
+
 u = uv(1); v = uv(2);
-[d_min, cct] = min(sqrt((cie1960PlanckianLocusUv(:,1) - u).^2 + (cie1960PlanckianLocusUv(:,2) - v).^2));
+%
+uInd = floor(u / 0.01 + 1);
+vInd = floor(v / 0.01 + 1);
+minCct = min(min(cie1960Cct(vInd:vInd+1, uInd:uInd+1)));
+maxCct = max(max(cie1960Cct(vInd:vInd+1, uInd:uInd+1)));
+cctR = maxCct - minCct;
+%}
+%minCct = 1; maxCct = 25000;
+
+[d_min, cct] = min(sqrt((cie1960PlanckianLocusUv(minCct:maxCct,1) - u).^2 + (cie1960PlanckianLocusUv(minCct:maxCct,2) - v).^2));
 
 end
 

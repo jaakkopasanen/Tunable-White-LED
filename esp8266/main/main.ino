9d5dfa10f9c486d89304ce9bcc3fadd97f17fd66
jaakko <jaakko.o.pasanen@gmail.com>
2016-05-05 19:24:20 +0300

    Reworked WebUI

--- esp8266/main/main.ino
+++ esp8266/main/main.ino
@@ -16,13 +16,7 @@ const int led1Pin_   = 5;
 const int led2Pin_   = 1;
 const int pwmRange_ = 1023;
 
-const char HTML_HEAD[] PROGMEM = "<!DOCTYPE html><html><head> <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-const char HTML_STYLE[] PROGMEM = "<style>body{background: #333; margin: 16px;}.container{max-width: 600px; margin: 0 auto;}.button-group{width: 100%; height: 56px; margin-bottom: 16px;}button{display: block; float: left; width: 50%; height: 56px; color: white; border: none;}button:hover{cursor: pointer;}#on-button{background-color: #1E7B1E; border-top-right-radius: 2px; border-bottom-right-radius: 2px;}#off-button{background-color: #CC1F00; border-top-left-radius: 2px; border-bottom-left-radius: 2px;}#on-button.active{background-color: #5BD75B;}#off-button.active{background-color: #FF6347;}.img{width: 100%; border-radius: 2px; position: relative; overflow: hidden;}.cursor{position: absolute; top: 50%; left: -10px; width: 6px; height: 6px; border-radius: 50%; transform: translate(-50%, -50%); background: black;}#dimming{height: 56px; margin-bottom: 16px; background: linear-gradient(to right, #000000 0%, #ffffff 100%);}#dimming-cursor{background: red;}#color-temperature{background-repeat: repeat-y; background-size: contain; height: 56px; margin-bottom: 16px;}</style>";
-const char HTML_HEAD_END[] PROGMEM = "</head";
-const char HTML_BODY[] PROGMEM = "<body> <div class=\"container\"> <div class=\"button-group\"> <button id=\"off-button\" type=\"button\">OFF</button> <button id=\"on-button\" type=\"button\">ON</button> </div><div id=\"dimming\" class=\"img\"><div id=\"dimming-cursor\" class=\"cursor\"></diV></div><div id=\"color-temperature\" class=\"img\" style=\"background-image: url('https://raw.githubusercontent.com/jaakkopasanen/Tunable-White-LED/master/Matlab/img/colorTemperatureChart_1000K_10000K.jpg');\"> <div id=\"color-temperature-cursor\" class=\"cursor\"></div></div><div id=\"cie1976ucs\" class=\"img\"> <img class=\"img\" src=\"https://raw.githubusercontent.com/jaakkopasanen/Tunable-White-LED/master/Matlab/img/CIE_1976_UCS_06_063_Full.jpg\"> <div id=\"cie1976ucs-cursor\" class=\"cursor\"></div></div></div></body>";
-const char HTML_JSONP[] PROGMEM = "<script type=\"text/javascript\">var onOff_ = {{onOff_}};var L_ = {{L_}};var u_ = {{u_}};var v_ = {{v_}};</script>";
-const char HTML_SCRIPT[] PROGMEM = "<script type=\"text/javascript\">var onButton=document.getElementById('on-button'); var offButton=document.getElementById('off-button'); var dimming=document.getElementById('dimming'); var dimmingCursor=document.getElementById('dimming-cursor'); var cie1976Ucs=document.getElementById('cie1976ucs'); var cie1976UcsCursor=document.getElementById('cie1976ucs-cursor'); var colorTemperature=document.getElementById('color-temperature'); var colorTemperatureCursor=document.getElementById('color-temperature-cursor'); var ajax=function (url){console.log('AJAX:', url); var xhr=new XMLHttpRequest(); xhr.open('GET', url, true); xhr.send(); xhr.onreadystatechange=function (){if (xhr.readyState===4){if (xhr.status===200){console.log('Success:', url);}else{console.log('Error:', xhr.responseText);}}};}; var setOnOff=function (on, doAjax){if (on){onButton.className='active'; offButton.className=''; if (doAjax) ajax('/on');}else{offButton.className='active'; onButton.className=''; if (doAjax) ajax('/off');}}; setOnOff(onOff_); onButton.addEventListener('click', function (){setOnOff(true, true);}); offButton.addEventListener('click', function (){setOnOff(false, true);}); var setDimming=function (L, doAjax){L_=L; dimmingCursor.style.left=L + '%'; if (doAjax) ajax('/cie1976Ucs?L=' + L + '&u=' + u_ + '&v=' + v_);}; dimming.addEventListener('click', function (ev){var L=(ev.pageX - this.offsetLeft) / dimming.offsetWidth * 100; setDimming(L, true);}); var setCie1976Ucs=function (u, v, doAjax){u_=u; v_=v; cie1976UcsCursor.style.left=(u / 0.63 * 100) + '%'; cie1976UcsCursor.style.top=((1 - v / 0.6) * 100) + '%'; unsetColorTemperature(); if (doAjax) ajax('/cie1976Ucs?L=' + L_ + '&u=' + u + '&v=' + v);}; var unsetCie1976Ucs=function (){cie1976UcsCursor.style.left='-10%'; cie1976UcsCursor.style.top='-10%';}; cie1976Ucs.addEventListener('click', function (ev){var u=(ev.pageX - this.offsetLeft) / cie1976Ucs.offsetWidth * 0.63; var v=(1 - (ev.pageY - this.offsetTop) / cie1976Ucs.offsetHeight) * 0.6; setCie1976Ucs(u, v, true);}); var setColorTemperature=function (T, doAjax){colorTemperatureCursor.style.left=((T - 1000) / 9000 * 100) + '%'; unsetCie1976Ucs(); if (doAjax) ajax('/colorTemperature?T=' + T);}; var unsetColorTemperature=function (){colorTemperatureCursor.style.left='-10%';}; colorTemperature.addEventListener('click', function (ev){var T=(ev.pageX - this.offsetLeft) / colorTemperature.offsetWidth * 9000 + 1000; setColorTemperature(T, true);}); setCie1976Ucs(u_, v_, false); setDimming(L_, false);</script>";
-const char HTML_END[] PROGMEM = "</html>";
+const char HTML_INDEX[] PROGMEM = "<!DOCTYPE html><html><head> <meta name='viewport' content='width=device-width, initial-scale=1.0'> <style>body{background: #333333; margin: 0;}.container{max-width: 500px; margin: 8px auto;}.layout-column, .layout-row{box-sizing: border-box; display: -webkit-flex; display: -ms-flexbox; display: flex;}.layout-row{-webkit-flex-direction: row; -ms-flex-direction: row; flex-direction: row;}.layout-column{-webkit-flex-direction: column; -ms-flex-direction: column; flex-direction: column; margin: 0 8px;}.layout-column > *:first-child{margin-bottom: 8px;}.flex{-webkit-flex: 1; -ms-flex: 1; flex: 1; box-sizing: border-box;}.button-group{width: 100%; height: 48px; margin-bottom: 8px;}button{display: block; width: 48px; height: 48px; color: white; border: none; border-radius: 2px;}button:hover{cursor: pointer;}#on-button{background-color: hsl(120, 75%, 20%);}#off-button{background-color: hsl(9, 100%, 25%);}#on-button.active{background-color: hsl(120, 75%, 50%);}#off-button.active{background-color: hsl(9, 100%, 60%);}.img{width: 100%; border-radius: 2px; position: relative; overflow: hidden;}.cursor{position: absolute; top: -10%; left: 50%; width: 6px; height: 6px; border-radius: 50%; transform: translate(-50%, -50%); background: black;}#dimming{width: 48px; background: linear-gradient(to bottom, #000000 5%, #ffffff 95%);}#dimming-cursor{background: red;}#color-temperature{background-repeat: repeat-x; background-size: contain; width: 48px;}#cie1976ucs{margin-bottom: 4px;}#cie1976ucs-zoom{height: 200px; background-color: #333333;}#cie1976ucs-zoom > img{position: absolute; top: 50%; bottom: auto; left: 50%; right: auto; transform-origin: 0% 0%; transform: scale(2);}</style></head><body> <div class='container layout-row'> <div class='layout-column'> <div id='dimming' class='img flex'><div id='dimming-cursor' class='cursor'></diV></div><button id='off-button' type='button'>OFF</button> </div><div class='flex layout-colum'> <div id='cie1976ucs' class='img flex'> <img class='img' src='https://raw.githubusercontent.com/jaakkopasanen/Tunable-White-LED/master/Matlab/img/CIE_1976_UCS_06_063_Full.jpg'> <div id='cie1976ucs-cursor' class='cursor'></div></div><div id='cie1976ucs-zoom' class='img'> <img id='cie1976ucs-zoom-image' class='img' src='https://raw.githubusercontent.com/jaakkopasanen/Tunable-White-LED/master/Matlab/img/CIE_1976_UCS_06_063_Full.jpg'> </div></div><div class='layout-column'> <div id='color-temperature' class='img flex' style='background-image: url(https://raw.githubusercontent.com/jaakkopasanen/Tunable-White-LED/master/Matlab/img/cct_1000K_to_10000K_pow2_vertical.jpg);'> <div id='color-temperature-cursor' class='cursor'></div></div><button id='on-button' type='button'>ON</button> </div></div></body><script type='text/javascript'>var onOff_={{onOff_}};var L_={{L_}};var u_={{u_}};var v_={{v_}};var T_={{T_}};debug=false;/*var onOff_=true;var L_=75;var u_=0.199;var v_=0.471;var T_=-1;var debug=true;*/</script><script type='text/javascript'> var onButton=document.getElementById('on-button'); var offButton=document.getElementById('off-button'); var dimming=document.getElementById('dimming'); var dimmingCursor=document.getElementById('dimming-cursor'); var cie1976Ucs=document.getElementById('cie1976ucs'); var cie1976UcsCursor=document.getElementById('cie1976ucs-cursor'); var cie1976UcsZoom=document.getElementById('cie1976ucs-zoom'); var cie1976UcsZoomImage=document.getElementById('cie1976ucs-zoom-image'); var colorTemperature=document.getElementById('color-temperature'); var colorTemperatureCursor=document.getElementById('color-temperature-cursor'); var mode='color'; if (T_ && !L_) mode='temperature'; var ajax=function (url){console.log('AJAX:', url); if (debug) return; var xhr=new XMLHttpRequest(); xhr.open('GET', url, true); xhr.send(); xhr.onreadystatechange=function (){if (xhr.readyState===4){if (xhr.status===200){console.log('Success:', url);}else{console.log('Error:', xhr.responseText);}}};}; var setOnOff=function (on, doAjax){if (on){onButton.className='active'; offButton.className=''; if (doAjax) ajax('/on');}else{offButton.className='active'; onButton.className=''; if (doAjax) ajax('/off');}}; setOnOff(onOff_); onButton.addEventListener('click', function (){setOnOff(true, true);}); offButton.addEventListener('click', function (){setOnOff(false, true);}); var setDimming=function (L, doAjax){L_=L; dimmingCursor.style.top=(L/100 * 90 + 5) + '%'; if (doAjax){if (mode==='color'){ajax('/cie1976Ucs?L=' + L_ + '&u=' + u_ + '&v=' + v_);}else{ajax('/colorTemperature?L=' + L_ + '&T=' + T_);}}}; dimming.addEventListener('click', function (ev){var y=(ev.pageY - dimming.offsetTop) / dimming.offsetHeight; y=(y - 0.05) / 0.9; y=Math.min(y, 1); y=Math.max(y, 0); var L=y * 100; setDimming(L, true);}); var setCie1976Ucs=function (u, v, doAjax){u_=u; v_=v; mode='color'; cie1976UcsCursor.style.left=(u / 0.63 * 100) + '%'; cie1976UcsCursor.style.top=((1 - v / 0.6) * 100) + '%'; x=-(u / 0.63) * cie1976Ucs.offsetWidth; y=-(1 - v / 0.6) * cie1976Ucs.offsetHeight; cie1976UcsZoomImage.style.transform='scale(4) translate(' + x + 'px, ' + y + 'px)'; unsetColorTemperature(); if (doAjax) ajax('/cie1976Ucs?L=' + L_ + '&u=' + u + '&v=' + v);}; var unsetCie1976Ucs=function (){cie1976UcsCursor.style.left='-10%'; cie1976UcsCursor.style.top='-10%';}; cie1976Ucs.addEventListener('click', function (ev){var u=(ev.pageX - cie1976Ucs.offsetLeft) / cie1976Ucs.offsetWidth * 0.63; var v=(1 - (ev.pageY - cie1976Ucs.offsetTop) / cie1976Ucs.offsetHeight) * 0.6; setCie1976Ucs(u, v, true);}); cie1976UcsZoom.addEventListener('click', function (ev){var dx=((ev.pageX - cie1976UcsZoom.offsetLeft) / cie1976UcsZoom.offsetWidth - 0.5); var dy=((ev.pageY - cie1976UcsZoom.offsetTop) / cie1976UcsZoom.offsetHeight - 0.5); var dxpx=dx * cie1976UcsZoom.offsetWidth; var dypx=dy * cie1976UcsZoom.offsetHeight; var du=dxpx / cie1976Ucs.offsetWidth * 0.63 / 4; var dv=dypx / cie1976Ucs.offsetHeight * 0.6 / 4; setCie1976Ucs(u_ + du, v_ - dv, true);}); var setColorTemperature=function (T, doAjax){T_=T; mode='temperature'; colorTemperatureCursor.style.top=((1 - Math.pow(((T - 1000) / 9000),0.5)) * 100) + '%'; unsetCie1976Ucs(); if (doAjax) ajax('/colorTemperature?L=' + L_ + '&T=' + T_);}; var unsetColorTemperature=function (){colorTemperatureCursor.style.top='-10%';}; colorTemperature.addEventListener('click', function (ev){var y=(ev.pageY - colorTemperature.offsetTop) / colorTemperature.offsetHeight; var T=Math.pow((1-y), 2) * 9000 + 1000; setColorTemperature(T, true);}); window.onload=function (){cie1976UcsZoom.style.height=cie1976Ucs.offsetWidth + 'px'; setCie1976Ucs(u_, v_, false); setDimming(L_, false);};</script></html>";
 
 ESP8266WebServer server(80);
 
@@ -195,14 +189,18 @@ void setCie1976Ucs (Cie1976Ucs luv) {
 /**
  * Sets ligth by color temperature
  */
-void setColorTemperature (int T) {
+void setColorTemperature (float L, int T) {
   if (!getOnOff()) return;
   
   // These cryptic looking formulas are a result of least RMSE fit of
   // CIE1976UCS coordinates vs color temperature
-  float u = (8.455e-7*pow(T,2) + 0.1525*T + 270) / (T - 85.02);
-  float v = (1.043e-6*pow(T,3) + 0.1475*pow(T,2) + 312.9*T + 0.7988) / (pow(T,2) + 18.76*T - 3.092);
+  double x = (T-5500.0)/2599.0; // Transform to z-score to avoid floating point precision problems
+  double u = (-0.0001747*pow(x,3.0) + 0.1833*pow(x,2.0) + 0.872*x + 1.227) / (pow(x,2.0) + 4.813*x + 5.933);
+  double v = (0.000311*pow(x,4.0) + 0.0009124*pow(x,3.0) + 0.3856*pow(x,2.0) + 1.873*x + 2.619) / (pow(x,2.0) + 4.323*x + 5.485);
+  
   Cie1976Ucs luv = {luv_.L, u, v};
+  if (L > 0) luv.L = L;
+  
   setCie1976Ucs(luv);
 
   // Save color temperature
@@ -214,18 +212,12 @@ void setColorTemperature (int T) {
  */
 void httpIndexController () {
   
-  String html = FPSTR(HTML_HEAD);
-  html += FPSTR(HTML_STYLE);
-  html += FPSTR(HTML_HEAD_END);
-  html += FPSTR(HTML_BODY);
-  html += FPSTR(HTML_JSONP);
-  html += FPSTR(HTML_SCRIPT);
-  html += FPSTR(HTML_END);
-
+  String html = FPSTR(HTML_INDEX);
   html.replace("{{onOff_}}", onOff_ ? "true" : "false");
   html.replace("{{L_}}", String(luv_.L));
   html.replace("{{u_}}", String(luv_.u));
   html.replace("{{v_}}", String(luv_.v));
+  html.replace("{{T_}}", String(T_));
   
   server.send(200, "text/html", html);
 }
@@ -399,25 +391,34 @@ void httpCie1976UcsController () {
 }
 
 void httpColorTemperatureController () {
-  int T = -1;
+  int T = -1; float L = -1;
   // Parse args
   for (uint8_t i = 0; i < server.args(); ++i) {
     if (server.argName(i) == "T") {
       T = server.arg(i).toInt();
+    } else if (server.argName(i) == "L") {
+      L = server.arg(i).toFloat();
     }
   }
 
   // Set color temperature
   if (T > 0) {
-    setColorTemperature(T);
+    setColorTemperature(L, T);
   } else {
     T = getColorTemperature();
+  }
+
+  // Lightness not given -> read from luv
+  if (L < 0) {
+    Cie1976Ucs luv = getCie1976Ucs();
+    L = luv.L;
   }
 
   // JSON response
   StaticJsonBuffer<20> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
   json["T"] = T;
+  json["L"] = L;
   String response;
   json.prettyPrintTo(response);
 
@@ -462,6 +463,10 @@ void setup(void){
   // Set onboard leds off as a sign for STA mode
   setLed1(false);
   setLed2(false);
+
+  // Set default to 2700K
+  setOnOff(true);
+  setColorTemperature(75, 2700);
 
   // Start Multicast domain name service
   //MDNS.begin("esp8266");
